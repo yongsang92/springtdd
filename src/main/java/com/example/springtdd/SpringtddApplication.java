2d10ddc65d75cb251521bcdd909c9ad61c570f2a
용 <yong@yong-ui-MacBookAir.local>
2022-02-22 18:04:31 +0900

    Initial commit for springtdd

---
+++ src/main/java/com/example/springtdd/SpringtddApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtdd/SpringtddApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+
+	}
+
+}
